3592e0052d7040093785f82dfc84127fa689997a
LiangJianghao <llmons@llmons-MacAir.local>
2025-06-10 23:11:45 +0800

    Initial commit

---
+++ cmd/server/wire/wire.go
@@ -0,0 +1,42 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-dianping/internal/handler"
+	"go-dianping/internal/repository"
+	"go-dianping/internal/server"
+	"go-dianping/internal/service"
+	"go-dianping/pkg/log"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var ServerSet = wire.NewSet(server.NewServerHTTP)
+
+var RepositorySet = wire.NewSet(
+	repository.NewDb,
+	repository.NewRepository,
+	repository.NewUserRepository,
+)
+
+var ServiceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var HandlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+func NewWire(*viper.Viper, *log.Logger) (*gin.Engine, func(), error) {
+	panic(wire.Build(
+		ServerSet,
+		RepositorySet,
+		ServiceSet,
+		HandlerSet,
+	))
+}
---
+++ internal/server/http.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-dianping/internal/handler"
+	"go-dianping/internal/middleware"
+	"go-dianping/pkg/log"
+	"net/http"
+)
+
+func NewServerHTTP(
+	logger *log.Logger,
+	userHandler *handler.UserHandler,
+) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.Use(
+		middleware.CORSMiddleware(),
+	)
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "OK")
+	})
+	r.GET("/user", userHandler.GetUserById)
+
+	return r
+}
