ff7036ff6bebb8278e7680f107d20dca025afa2b
llmons <llmons@foxmail.com>
2025-07-08 14:25:43 +0800

    feat: implement LikeBlog handler and update blog service for liking posts

--- internal/handler/blog.go
+++ internal/handler/blog.go
@@ -52,20 +52,29 @@ func (h *BlogHandler) SaveBlog(ctx *gin.Context) {
 	v1.HandleSuccess(ctx, blog.ID)
 }
 
+// LikeBlog godoc
+// @Summary 点赞博文
+// @Schemes
+// @Description
+// @Tags shop
+// @Accept json
+// @Produce json
+// @Param id path uint64 true "博文 ID"
+// @Success 200 {object} v1.QueryShopByIDResp
+// @Router /blog/like/{id} [put]
 func (h *BlogHandler) LikeBlog(ctx *gin.Context) {
-	var blog model.Blog
-	if err := ctx.ShouldBindJSON(&blog); err != nil {
+	var req struct {
+		ID uint64 `uri:"id" binding:"required"`
+	}
+	if err := ctx.ShouldBindUri(&req); err != nil {
 		v1.HandleError(ctx, http.StatusBadRequest, err.Error(), nil)
 		return
 	}
-	// 获取登录用户
-	user := user_holder.GetUser(ctx)
-	blog.ID = *user.ID
-	//	保存探店博文
-	if err := h.blogService.SaveBlog(ctx, &blog); err != nil {
+
+	if err := h.blogService.LikeBlog(ctx, req.ID); err != nil {
 		v1.HandleError(ctx, http.StatusInternalServerError, err.Error(), nil)
 		return
 	}
 	//	返回 id
-	v1.HandleSuccess(ctx, blog.ID)
+	v1.HandleSuccess(ctx, nil)
 }
--- internal/service/blog.go
+++ internal/service/blog.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"context"
+	"go-dianping/internal/base/user_holder"
 	"go-dianping/internal/model"
 )
 
 type BlogService interface {
 	SaveBlog(ctx context.Context, blog *model.Blog) error
+	LikeBlog(ctx context.Context, id uint64) error
 }
 
 func NewBlogService(
@@ -23,4 +25,12 @@ type blogService struct {
 
 func (s *blogService) SaveBlog(_ context.Context, blog *model.Blog) error {
 	return s.query.Blog.Save(blog)
+}
+
+func (s *blogService) LikeBlog(ctx context.Context, id uint64) error {
+	//	1. 获取登录用户
+	userId := user_holder.GetUser(ctx).ID
+	//	2. 判断当前登录用户是否已经点赞
+	// TODO implement like logic
+	return nil
 }
